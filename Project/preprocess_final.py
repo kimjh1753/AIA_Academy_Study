cad112b51e4c167ebab48837ac00db34c113f9e8
kimjh1753 <kimjh1753@naver.com>
2021-02-20 17:52:03 +0900

    change

--- Project/preprocess_final.py
+++ Project/preprocess_final.py
@@ -3,76 +3,76 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage.transform import pyramid_reduce
 
-# 경로
-base_path = r'C:\project\celeba-dataset'   
-img_base_path = os.path.join(base_path, 'img_align_celeba2') 
-target_img_path = os.path.join(base_path, 'processed')
+# # 경로
+# base_path = r'C:\project\celeba-dataset'   
+# img_base_path = os.path.join(base_path, 'img_align_celeba2') 
+# target_img_path = os.path.join(base_path, 'processed')
 
-eval_list = np.loadtxt(os.path.join(base_path, 'list_eval_partition.csv'), 
-                       dtype=str, 
-                       delimiter=',', 
-                       skiprows=1)
+# eval_list = np.loadtxt(os.path.join(base_path, 'list_eval_partition.csv'), 
+#                        dtype=str, 
+#                        delimiter=',', 
+#                        skiprows=1)
 
-print(eval_list[0]) # ['000001.jpg' '0']
+# print(eval_list[0]) # ['000001.jpg' '0']
 
-# 이미지 확인
-img_sample = cv2.imread(os.path.join(img_base_path, 
-                                     eval_list[0][0]))
+# # 이미지 확인
+# img_sample = cv2.imread(os.path.join(img_base_path, 
+#                                      eval_list[0][0]))
 
-h, w, _ = img_sample.shape
+# h, w, _ = img_sample.shape
 
-#이미지 전처리
+# #이미지 전처리
 
-# 정사각형 이미지로 crop 해준다.
-crop_sample = img_sample[int((h-w)/2):int(-(h-w)/2), :]
+# # 정사각형 이미지로 crop 해준다.
+# crop_sample = img_sample[int((h-w)/2):int(-(h-w)/2), :]
 
-# 이미지를 4배만큼 축소하고 normalize(정규화) 한다. ex) downscale=4 : 원본 사진 대비 4배 축소됨
-resized_sample = pyramid_reduce(crop_sample, 
-                                downscale=4,
-                                multichannel=True) # multichannel=True -> 컬러채널 허용
+# # 이미지를 4배만큼 축소하고 normalize(정규화) 한다. ex) downscale=4 : 원본 사진 대비 4배 축소됨
+# resized_sample = pyramid_reduce(crop_sample, 
+#                                 downscale=4,
+#                                 multichannel=True) # multichannel=True -> 컬러채널 허용
 
-print(crop_sample.shape) # (178, 178, 3)
+# print(crop_sample.shape) # (178, 178, 3)
 
-plt.figure(figsize=(12, 5)) # 최초 창의 크기를 가로 12인치 세로 5인치로 설정
-plt.subplot(1, 3, 1)        # 1행 3열 중 첫번째
-plt.imshow(img_sample)      # 기존 이미지
-plt.subplot(1, 3, 2)        # 1행 3열 중 두번째
-plt.imshow(crop_sample)     # 기존 이미지를 정사각형으로 자른 이미지
-plt.subplot(1, 3, 3)        # 1행 3열 중 세번째
-plt.imshow(resized_sample)  # crop 이미지를 4배 축소한 이미지
-plt.show()
+# plt.figure(figsize=(12, 5)) # 최초 창의 크기를 가로 12인치 세로 5인치로 설정
+# plt.subplot(1, 3, 1)        # 1행 3열 중 첫번째
+# plt.imshow(img_sample)      # 기존 이미지
+# plt.subplot(1, 3, 2)        # 1행 3열 중 두번째
+# plt.imshow(crop_sample)     # 기존 이미지를 정사각형으로 자른 이미지
+# plt.subplot(1, 3, 3)        # 1행 3열 중 세번째
+# plt.imshow(resized_sample)  # crop 이미지를 4배 축소한 이미지
+# plt.show()
 
-# main
-downscale = 4
-n_train = 18650
-n_val = 5700
-n_test = 5649
+# # main
+# downscale = 4
+# n_train = 162770
+# n_val = 19867
+# n_test = 19962
 
-for i, e in enumerate(eval_list):
-    filename, ext = os.path.splitext(e[0]) # os.path.splitext : 확장자만 따로 분류한다.(리스트로 나타낸다)
+# for i, e in enumerate(eval_list):
+#     filename, ext = os.path.splitext(e[0]) # os.path.splitext : 확장자만 따로 분류한다.(리스트로 나타낸다)
     
-    img_path = os.path.join(img_base_path, e[0]) 
+#     img_path = os.path.join(img_base_path, e[0]) 
     
-    img = cv2.imread(img_path, cv2.IMREAD_COLOR) # img_path에 있는 있는 이미지를 부른다.
+#     img = cv2.imread(img_path, cv2.IMREAD_COLOR) # img_path에 있는 있는 이미지를 부른다.
     
-    h, w, _ = img.shape
+#     h, w, _ = img.shape
     
-    crop = img[int((h-w)/2):int(-(h-w)/2), :] # 정사각형 이미지로 crop 해준다.
-    crop = cv2.resize(crop, dsize=(176,176)) # dsize=(176,176) : 결과 이미지 크기는 Tuple형을 사용하고 176, 176으로 변경
-    # 이미지를 4배만큼 축소하고 normalize(정규화) 한다. ex) downscale=4 : 원본 사진 대비 4배 축소됨
-    resized = pyramid_reduce(crop, downscale=downscale, multichannel=True) # multichannel=True -> 컬러채널 허용
+#     crop = img[int((h-w)/2):int(-(h-w)/2), :] # 정사각형 이미지로 crop 해준다.
+#     crop = cv2.resize(crop, dsize=(176,176)) # dsize=(176,176) : 결과 이미지 크기는 Tuple형을 사용하고 176, 176으로 변경
+#     # 이미지를 4배만큼 축소하고 normalize(정규화) 한다. ex) downscale=4 : 원본 사진 대비 4배 축소됨
+#     resized = pyramid_reduce(crop, downscale=downscale, multichannel=True) # multichannel=True -> 컬러채널 허용
     
-    norm = cv2.normalize(crop.astype(np.float64), None, 0, 1, cv2.NORM_MINMAX) # -> 이미지를 0과 1 사이로 normalize(정규화)함
+#     norm = cv2.normalize(crop.astype(np.float64), None, 0, 1, cv2.NORM_MINMAX) # -> 이미지를 0과 1 사이로 normalize(정규화)함
      
-    if int(e[1]) == 0: # Train
-        np.save(os.path.join(target_img_path, 'x_train', filename + '.npy'), resized)
-        np.save(os.path.join(target_img_path, 'y_train', filename + '.npy'), norm)
-    elif int(e[1]) == 1: # Validation
-        np.save(os.path.join(target_img_path, 'x_val', filename + '.npy'), resized)
-        np.save(os.path.join(target_img_path, 'y_val', filename + '.npy'), norm)
-    elif int(e[1]) == 2: # Test
-        np.save(os.path.join(target_img_path, 'x_test', filename + '.npy'), resized)
-        np.save(os.path.join(target_img_path, 'y_test', filename + '.npy'), norm)
+#     if int(e[1]) == 0: # Train
+#         np.save(os.path.join(target_img_path, 'x_train', filename + '.npy'), resized)
+#         np.save(os.path.join(target_img_path, 'y_train', filename + '.npy'), norm)
+#     elif int(e[1]) == 1: # Validation
+#         np.save(os.path.join(target_img_path, 'x_val', filename + '.npy'), resized)
+#         np.save(os.path.join(target_img_path, 'y_val', filename + '.npy'), norm)
+#     elif int(e[1]) == 2: # Test
+#         np.save(os.path.join(target_img_path, 'x_test', filename + '.npy'), resized)
+#         np.save(os.path.join(target_img_path, 'y_test', filename + '.npy'), norm)
 
 # ============================================================================================================= 
     
@@ -80,17 +80,16 @@ for i, e in enumerate(eval_list):
 # second_path = '.npy'
 # train_list = []
 
-# for i in range(1, 301):
+# for i in range(1, 8001):
 #     i = '{0:06d}'.format(i)
 #     train_path = x_train_path + i + second_path
-#     # print(x_train_path.format(train_path))
 #     a = np.load(train_path)
 #     train_list.append(a)
 # x_train = np.array(train_list).reshape(-1,44,44,3)
 # np.save(x_train_path, arr=x_train)
 
 # x_train = np.load('../project/celeba-dataset/processed/x_train/x_train.npy')
-# print(x_train.shape) # (300, 44, 44, 3)
+# print(x_train.shape) # (8000, 44, 44, 3)
 
 # ===================================================================
 
@@ -98,86 +97,85 @@ for i, e in enumerate(eval_list):
 # second_path = '.npy'
 # train_list = []
 
-# for i in range(1, 301):
+# for i in range(1, 8001):
 #     i = '{0:06d}'.format(i)
 #     train_path = y_train_path + i + second_path
-#     # print(x_train_path.format(train_path))
 #     a = np.load(train_path)
 #     train_list.append(a)
 # y_train = np.array(train_list).reshape(-1,176,176,3)
 # np.save(y_train_path, arr=y_train)
 
 # y_train = np.load('../project/celeba-dataset/processed/y_train/y_train.npy')
-# print(y_train.shape) # (300, 176, 176, 3)
+# print(y_train.shape) # (8000, 176, 176, 3)
 
 # ===================================================================
 
 # x_val_path = '../project/celeba-dataset/processed/x_val/'
 # second_path = '.npy'
-# train_list = []
+# val_list = []
 
-# for i in range(18651, 18951):
+# for i in range(162771, 163771):
 #     i = '{0:06d}'.format(i)
-#     train_path = x_val_path + i + second_path
-#     # print(x_train_path.format(train_path))
-#     a = np.load(train_path)
-#     train_list.append(a)
-# x_val = np.array(train_list).reshape(-1,44,44,3)
+#     val_path = x_val_path + i + second_path
+#     a = np.load(val_path)
+#     val_list.append(a)
+# x_val = np.array(val_list).reshape(-1,44,44,3)
 # np.save(x_val_path, arr=x_val)
 
 # x_val = np.load('../project/celeba-dataset/processed/x_val/x_val.npy')
-# print(x_val.shape) # (300, 44, 44, 3)
+# print(x_val.shape) # (1000, 44, 44, 3)
 
 # ===================================================================
 
 # y_val_path = '../project/celeba-dataset/processed/y_val/'
 # second_path = '.npy'
-# train_list = []
+# val_list = []
 
-# for i in range(18651, 18951):
+# for i in range(162771, 163771):
 #     i = '{0:06d}'.format(i)
-#     train_path = y_val_path + i + second_path
-#     # print(x_train_path.format(train_path))
-#     a = np.load(train_path)
-#     train_list.append(a)
-# y_val = np.array(train_list).reshape(-1,176,176,3)
+#     val_path = y_val_path + i + second_path
+#     a = np.load(val_path)
+#     val_list.append(a)
+# y_val = np.array(val_list).reshape(-1,176,176,3)
 # np.save(y_val_path, arr=y_val)
 
 # y_val = np.load('../project/celeba-dataset/processed/y_val/y_val.npy')
-# print(y_val.shape) # (300, 176, 176, 3)
+# print(y_val.shape) # (1000, 176, 176, 3)
 
 # ===================================================================
 
 # x_test_path = '../project/celeba-dataset/processed/x_test/'
 # second_path = '.npy'
-# train_list = []
+# test_list = []
 
-# for i in range(24351, 24651):
+# for i in range(182638, 183638):
 #     i = '{0:06d}'.format(i)
-#     train_path = x_test_path + i + second_path
+#     test_path = x_test_path + i + second_path
 #     # print(x_train_path.format(train_path))
-#     a = np.load(train_path)
-#     train_list.append(a)
-# x_test = np.array(train_list).reshape(-1,44,44,3)
+#     a = np.load(test_path)
+#     test_list.append(a)
+# x_test = np.array(test_list).reshape(-1,44,44,3)
 # np.save(x_test_path, arr=x_test)
 
 # x_test = np.load('../project/celeba-dataset/processed/x_test/x_test.npy')
-# print(x_test.shape) # (300, 44, 44, 3)
+# print(x_test.shape) # (1000, 44, 44, 3)
 
 # ===================================================================
 
 # y_test_path = '../project/celeba-dataset/processed/y_test/'
 # second_path = '.npy'
-# train_list = []
+# test_list = []
 
-# for i in range(24351, 24651):
+# for i in range(182638, 183638):
 #     i = '{0:06d}'.format(i)
-#     train_path = y_test_path + i + second_path
-#     # print(x_train_path.format(train_path))
-#     a = np.load(train_path)
-#     train_list.append(a)
-# y_test = np.array(train_list).reshape(-1,176,176,3)
+#     test_path = y_test_path + i + second_path
+#     a = np.load(test_path)
+#     test_list.append(a)
+# y_test = np.array(test_list).reshape(-1,176,176,3)
 # np.save(y_test_path, arr=y_test)
 
 # y_test = np.load('../project/celeba-dataset/processed/y_test/y_test.npy')
-# print(y_test.shape) # (300, 176, 176, 3)
+# print(y_test.shape) # (1000, 176, 176, 3)
+
+
+
--- Project/train_final.py
+++ Project/train_final.py
@@ -12,158 +12,129 @@ from skimage.transform import pyramid_expand
 from Subpixel import Subpixel
 
 x_train = np.load('../project/celeba-dataset/processed/x_train/x_train.npy')
-x_val = np.load('../project/celeba-dataset/processed/x_val/x_val.npy')
 y_train = np.load('../project/celeba-dataset/processed/y_train/y_train.npy')
+
+x_val = np.load('../project/celeba-dataset/processed/x_val/x_val.npy')
 y_val = np.load('../project/celeba-dataset/processed/y_val/y_val.npy')
 
-print(x_train.shape, y_train.shape) # (300, 44, 44, 3) (300, 176, 176, 3)
-print(x_val.shape, y_val.shape) # (300, 44, 44, 3) (300, 176, 176, 3)
+x_test = np.load('../project/celeba-dataset/processed/x_test/x_test.npy')
+y_test = np.load('../project/celeba-dataset/processed/y_test/y_test.npy')
 
-x1 = x_train[0] 
-x2 = x_val[0]
+print(x_train.shape, y_train.shape) # (8000, 44, 44, 3) (8000, 176, 176, 3)
+print(x_val.shape, y_val.shape)     # (1000, 44, 44, 3) (1000, 176, 176, 3)
+print(x_test.shape, y_test.shape)   # (1000, 44, 44, 3) (1000, 176, 176, 3)
+
+x1 = x_train[11] 
+x2 = x_val[11]
 
 print(x1.shape, x2.shape) # (44, 44, 3) (44, 44, 3)
 
-plt.subplot(1, 2, 1)
-plt.imshow(x1)
-plt.subplot(1, 2, 2)
-plt.imshow(x2)
-plt.show()
+# plt.subplot(1, 2, 1)
+# plt.imshow(x1)
+# plt.subplot(1, 2, 2)
+# plt.imshow(x2)
+# plt.show()
 
 upscale_factor = 4
 
 inputs = Input(shape=(44, 44, 3))
-a = Conv2D(filters=64, kernel_size=5, strides=1, padding='same', activation='relu')(inputs)
-a = Conv2D(filters=64, kernel_size=3, strides=1, padding='same', activation='relu')(a)
-a = Conv2D(filters=32, kernel_size=3, strides=1, padding='same', activation='relu')(a)
-a = Conv2D(filters=16, kernel_size=3, strides=1, padding='same', activation='relu')(a)
-a = Subpixel(filters=3, kernel_size=3, r=upscale_factor, padding='same')(a) # r = upscale_factor : (44, 44, 3) -> (176, 176, 3)
+a = Conv2D(filters=64, 
+           kernel_size=5, 
+           strides=1, 
+           padding='same', 
+           activation='tanh')(inputs)
+a = Conv2D(filters=64, 
+           kernel_size=3, 
+           strides=1, 
+           padding='same', 
+           activation='tanh')(a)
+a = Conv2D(filters=32, 
+           kernel_size=3, 
+           strides=1, 
+           padding='same', 
+           activation='tanh')(a)
+a = Conv2D(filters=16, 
+           kernel_size=3, 
+           strides=1, 
+           padding='same', 
+           activation='tanh')(a)
+a = Subpixel(filters=3, 
+             kernel_size=3, 
+             r=upscale_factor,  # r = upscale_factor : (44, 44, 3) -> (176, 176, 3)
+             padding='same')(a) 
 outputs = LeakyReLU(alpha=0.1)(a)
 model = Model(inputs=inputs, outputs=outputs)
 
 model.summary()
 
 cp = ModelCheckpoint('../project/celeba-dataset/models/model-final.h5', monitor='val_loss', verbose=1, save_best_only=True)
-es = EarlyStopping(monitor='val_loss', patience=40, verbose=1, mode='auto')
-rl = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=20, verbose=1, mode='auto')
 
 model.compile(loss='mse', optimizer='adam', metrics=['mae']) # loss='mse'-> 이미지가 얼마나 같은지, 픽셀 값이 얼마나 같은지 확인하기 위해서 mse사용
-history = model.fit(x_train, y_train, epochs=10, validation_data=[x_val, y_val], verbose=1, callbacks=[cp])
+history = model.fit(x_train, y_train, epochs=1000, validation_data=(x_val, y_val), verbose=1, callbacks=[cp])
 
-loss = history.history['loss']
-mae = history.history['mae']
+model.save('../project/celeba-dataset/h5/project_final.h5')
 
-print("loss : ", loss[-1])  
-print("mae : ", mae[-1])    
+result = model.evaluate(x_test, y_test)
 
-# x_test_list = sorted(glob.glob(os.path.join(base_path, 'x_test', '*.npy')))
-# y_test_list = sorted(glob.glob(os.path.join(base_path, 'y_test', '*.npy')))
+print("loss : ", result[0])
+print("mae : ", result[1])
 
-# # print(len(x_test_list), len(y_test_list))   # 5649 5649
-# # print(x_test_list[0])                       # C:\project\celeba-dataset\processed\x_test\024351.npy
+# plt.figure(figsize=(10, 6)) # 최초 창의 크기를 가로 10인치 세로 6인치로 설정
+# plt.suptitle('epochs = 100, activation = relu') # 제목 설정
+# plt.suptitle('epochs = 100, activation = relu, LeakyReLU(alpha=0.1)') # 제목 설정
+# plt.suptitle('epochs = 100, activation = linear') # 제목 설정
+# plt.suptitle('epochs = 100, activation = linear, LeakyReLU(alpha=0.1)') # 제목 설정
+# plt.suptitle('epochs = 100, activation = tanh') # 제목 설정
+# plt.suptitle('epochs = 100, activation = tanh, LeakyReLU(alpha=0.1)') # 제목 설정
 
-x_test = np.load('../project/celeba-dataset/processed/x_test/x_test.npy')
-y_test = np.load('../project/celeba-dataset/processed/y_test/y_test.npy')
+# plt.subplot(2, 1, 1)    # 2행 1열중 첫번째
+# plt.plot(history.history['loss'], marker='.', c='red', label='loss')
+# plt.plot(history.history['val_loss'], marker='.', c='blue', label='val_loss')
+# plt.grid()
 
-print(x_test.shape, y_test.shape) # (300, 44, 44, 3) (300, 176, 176, 3)
+# plt.ylabel('loss')
+# plt.xlabel('epoch')
+# plt.legend(loc='upper right')
 
-test_idx = 10
+# plt.subplot(2, 1, 2)    # 2행 1열중 첫번째
+# plt.plot(history.history['mae'], marker='.', c='red', label='loss')
+# plt.plot(history.history['val_mae'], marker='.', c='blue', label='val_mae')
+# plt.grid()
 
-# 저해상도 이미지(input)
-x1_test = x_test[test_idx]
-
-# 저해상도 이미지 확대시킨 이미지
-x1_test_resized = pyramid_expand(x1_test, 
-                                 4,
-                                 multichannel=True) # multichannel=True -> 컬러채널 허용
-
-# 정답 이미지
-y1_test = y_test[test_idx]
-
-# 모델이 예측한 이미지(output)
-y_pred = model.predict(x1_test.reshape((1, 44, 44, 3)))
-
-print(x1_test.shape, y1_test.shape) # (44, 44, 3) (176, 176, 3)
-
-# unit8 = 데이터 행렬의 클래스가 uint8 인 이미지를 8 비트 이미지, 이미지 기능은 8 비트 이미지를 배정 밀도로 변환하지 않고 직접 표시 할 수 있습니다.
-x1_test = (x1_test * 255).astype(np.uint8) 
-x1_test_resized = (x1_test_resized * 255).astype(np.uint8)
-
-y1_test = (y1_test * 255).astype(np.uint8)
-y_pred = np.clip(y_pred.reshape((176, 176, 3)), 0, 1) # ypred.reshape된 값을 0과 1 사이의 범위로 전환
-
-# input 이미지
-x1_test = cv2.cvtColor(x1_test, 
-                       cv2.COLOR_BGR2RGB) 
-                         # COLOR_BGR2RGB -> BGR 사진을 RGB 사진으로 변환, OpenCV에서는 BGR 순서로 저장하고 matplotlib에서는 RGB 순서로 저장이 되어서 변경해주는 역할
-
-# input 이미지를 4배 확대한 이미지
-x1_test_resized = cv2.cvtColor(x1_test_resized, 
-                               cv2.COLOR_BGR2RGB)
-                                 # COLOR_BGR2RGB -> BGR 사진을 RGB 사진으로 변환, OpenCV에서는 BGR 순서로 저장하고 matplotlib에서는 RGB 순서로 저장이 되어서 변경해주는 역할
-                                   
-# input 이미지로 예측한 이미지(저해상도 -> 고해상도)
-y_pred = cv2.cvtColor(y_pred, 
-                      cv2.COLOR_BGR2RGB)
-                        # COLOR_BGR2RGB -> BGR 사진을 RGB 사진으로 변환, OpenCV에서는 BGR 순서로 저장하고 matplotlib에서는 RGB 순서로 저장이 되어서 변경해주는 역할
-                         
-# 원본 이미지
-y1_test = cv2.cvtColor(y1_test, 
-                       cv2.COLOR_BGR2RGB)
-                         # COLOR_BGR2RGB -> BGR 사진을 RGB 사진으로 변환, OpenCV에서는 BGR 순서로 저장하고 matplotlib에서는 RGB 순서로 저장이 되어서 변경해주는 역할
-                           
-plt.figure(figsize=(12, 5))
-
-plt.subplot(1, 4, 1) # 1행 4열중 첫번째
-plt.title('input') # 저해상도 이미지
-plt.imshow(x1_test, interpolation='bicubic')
-
-plt.subplot(1, 4, 2) # 1행 4열중 첫번째
-plt.title('resized') # input 이미지를 강제로 4배 늘린 이미지
-plt.imshow(x1_test_resized, interpolation='bicubic')
-
-plt.subplot(1, 4, 3) # 1행 4열중 첫번째
-plt.title('output') # input 이미지를 Subpixel을 통해 예측한 이미지
-plt.imshow(y_pred, interpolation='bicubic')
-
-plt.subplot(1, 4, 4) # 1행 4열중 첫번째
-plt.title('groundtruth') # 원본 이미지
-plt.imshow(y1_test, interpolation='bicubic')
-
-plt.show()
-
-# activation 별로 비교
-# epochs = 2, activation = 'relu'
-# loss :  0.0016763228923082352
-# mae :  0.026696009561419487
-
-# epochs = 2, activation = 'relu', LeakyReLU(alpha=0.1)
-# loss :  0.0016761336009949446
-# mae :  0.026643605902791023
+# plt.ylabel('mae')
+# plt.xlabel('epoch')
+# plt.legend(loc='upper right')
+# plt.show()
 
 
-# epochs = 2, activation = 'linear'
-# loss :  0.0018620517803356051
-# mae :  0.027616102248430252
+# activation 별로 비교 
+# epochs = 100, activation = 'relu'
+# loss :  0.0013303515734151006
+# mae :  0.022961219772696495
 
-# epochs = 2, activation = 'linear', LeakyReLU(alpha=0.1)
-# loss :  0.006469338666647673
-# mae :  0.036886803805828094 
+# epochs = 100, activation = 'relu', LeakyReLU(alpha=0.1)
+# loss :  0.0012986732181161642
+# mae :  0.022124871611595154
 
 
-# epochs =2, activation = 'tanh'
-# loss :  0.0026562961284071207
-# mae :  0.036763615906238556
+# epochs = 100, activation = 'linear'
+# loss :  0.0017265279311686754
+# mae :  0.025380082428455353
 
-# epochs = 2, activation = 'than', LeakyReLU(alpha=0.1)
-# loss :  0.0027720301877707243
-# mae :  0.03674674406647682
+# epochs = 100, activation = 'linear', LeakyReLU(alpha=0.1)
+# loss :  0.0017905107233673334
+# mae :  0.026167450472712517
+
+
+# epochs = 100, activation = 'tanh'
+# loss :  0.0016838000155985355
+# mae :  0.02680797316133976
+
+# epochs = 100, activation = 'than', LeakyReLU(alpha=0.1)
+# loss :  0.0016121364897117019
+# mae :  0.025504857301712036
 
 # 최종 
-# epochs = 30, activation = 'relu', LeakyReLU(alpha=0.1)
-# loss :  0.0012984503991901875
-# mae :  0.022568069398403168
 
-# epochs = 10, activation = 'relu', LeakyReLU(alpha=0.1)
-# loss :  0.001365274772979319
-# mae :  0.023278996348381042
+
+
