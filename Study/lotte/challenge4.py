f905129a87b259783a4228b4533e4bbc418ecfe2
kimjh1753 <kimjh1753@naver.com>
2021-03-21 13:19:55 +0900

    change

--- Study/lotte/challenge4.py
+++ Study/lotte/challenge4.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from tensorflow import keras
 from keras.preprocessing.image import ImageDataGenerator
 from keras import Sequential
 from keras.layers import *
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.applications import EfficientNetB7, MobileNet, EfficientNetB4
+from tensorflow.keras.applications import EfficientNetB7, MobileNet, EfficientNetB4, MobileNetV2, InceptionResNetV2
 from tensorflow.keras.applications.efficientnet import preprocess_input
 from sklearn.model_selection import train_test_split
 
@@ -16,55 +17,60 @@ x = np.load("../study/LPD_COMPETITION/npy/P_project_x4.npy", allow_pickle=True)
 y = np.load("../study/LPD_COMPETITION/npy/P_project_y4.npy", allow_pickle=True)
 x_pred = np.load('../study/LPD_COMPETITION/npy/pred.npy', allow_pickle=True)
 
-print(x.shape, y.shape, x_pred.shape)   # (48000, 64, 64, 3) (48000, 1000) (72000, 64, 64, 3)
+print(x.shape, y.shape, x_pred.shape)   # (48000, 128, 128, 3) (48000, 1000) (72000, 128, 128, 3)
 
-idg = ImageDataGenerator(            
-    width_shift_range=(-1, 1),  # 지정된 수평방향 이동 범위내에서 임의로 원본이미지를 이동
-    height_shift_range=(-1, 1), # 지정된 수직방향 이동 범위내에서 임의로 원본이미지를 이동
-    rotation_range=5,          # 지정된 각도 범위내에서 임의로 원본이미지를 회전
-    zoom_range=0.2,             # 지정된 확대/축소 범위내에서 임의로 원본이미지를 확대/축소
-    shear_range=0.2,            # 밀림 강도 범위내에서 임의로 원본이미지를 변형
-    fill_mode='nearest'         # 빈자리를 채워준다
-)
+idg = ImageDataGenerator(
+    width_shift_range=(-1,1),   
+    height_shift_range=(-1,1),  
+    shear_range=0.2)    # 현상유지
 
 idg2 = ImageDataGenerator()
 
-y = np.argmax(y, axis=1)
+# y = np.argmax(y, axis=1)
 
 x = preprocess_input(x) 
 x_pred = preprocess_input(x_pred) 
 
-x_train, x_valid, y_train, y_valid = train_test_split(x, y, train_size = 0.8, shuffle = True, random_state=47)
+x_train, x_valid, y_train, y_valid = train_test_split(x, y, train_size = 0.8, shuffle = True, random_state=49)
 
 train_generator = idg.flow(x_train, y_train, batch_size=64, seed=2048)
 # seed => random_state
-valid_generator = idg2.flow(x_valid,y_valid, batch_size=64, seed=2048)
+valid_generator = idg2.flow(x_valid, y_valid)
 test_generator = x_pred
 
-from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import GlobalAveragePooling2D, Flatten, BatchNormalization, Dense, Activation
-EfficientNetB4 = EfficientNetB4(include_top=False, weights='imagenet', input_shape=x_train.shape[1:])
-EfficientNetB4.trainable = True
-a = EfficientNetB4.output
+from tensorflow.keras import regularizers
+from tensorflow.keras.models import Model, Sequential, load_model
+from tensorflow.keras.layers import GlobalAveragePooling2D, Flatten, BatchNormalization, Dense, Activation, Dropout, Conv2D
+InceptionResNetV2 = InceptionResNetV2(include_top=False, weights='imagenet', input_shape=x_train.shape[1:])
+# MobileNet = MobileNet(include_top=True, weights='imagenet')
+
+InceptionResNetV2.trainable = True
+a = InceptionResNetV2.output
+a = Conv2D(1000, 1, padding='same', activation='swish', 
+           kernel_regularizer=regularizers.l2(0.01),       # 1e-5 
+           activity_regularizer=regularizers.l1(0.01)) (a) # 1e-5           
 a = Flatten() (a)
+# a = Activation('swish')(a)
 a = Dense(1000, activation= 'softmax') (a)
 
-model = Model(inputs = EfficientNetB4.input, outputs = a)
+model = Model(inputs = InceptionResNetV2.input, outputs = a)
 
+# MobileNet.summary()
 model.summary()
 
-model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['acc'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 
 # 3. 컴파일 훈련
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
-mc = ModelCheckpoint('../study/LPD_COMPETITION/h5/challenge7.hdf5', save_only_true=True, verbose=1)
+mc = ModelCheckpoint('../study/LPD_COMPETITION/h5/challenge8.hdf5', save_only_true=True, verbose=1)
 es = EarlyStopping(patience=30)
 reduce_lr = ReduceLROnPlateau(patience=15, factor=0.5)
 
-model.fit_generator(train_generator, epochs=1000, validation_data=valid_generator, 
+model.fit_generator(train_generator, epochs=200, validation_data=valid_generator, 
                     callbacks=[es, reduce_lr, mc], steps_per_epoch= len(x_train) / 64)
 
-model.load_weights('../study/LPD_COMPETITION/h5/challenge7.hdf5')
+model.save('../study/LPD_COMPETITION/h5/challenge9.hdf5')
+# model = load_model('../study/LPD_COMPETITION/h5/challenge9.hdf5')
 
 # predict
 result = model.predict(test_generator,verbose=True)
@@ -72,5 +78,5 @@ result = model.predict(test_generator,verbose=True)
 print(result.shape)
 sub = pd.read_csv('../study/LPD_COMPETITION/sample.csv')
 sub['prediction'] = np.argmax(result, axis = 1)
-sub.to_csv('../study/LPD_COMPETITION/answer7.csv', index=False)
+sub.to_csv('../study/LPD_COMPETITION/answer/answer10.csv', index=False)
 
--- Study/lotte/challenge5.py
+++ Study/lotte/challenge5.py
@@ -4,20 +4,21 @@ import pandas as pd
 from tensorflow.keras.callbacks import EarlyStopping
 from keras.applications.resnet50 import preprocess_input
 
-x = np.load('../study/LPD_COMPETITION/npy/x_data.npy')
-y = np.load('../study/LPD_COMPETITION/npy/y_data.npy')
-x_pred = np.load('../study/LPD_COMPETITION/npy/pred.npy')
+#데이터 지정 및 전처리
+x = np.load("../study/LPD_COMPETITION/npy/P_project_x4.npy", allow_pickle=True)
+y = np.load("../study/LPD_COMPETITION/npy/P_project_y4.npy", allow_pickle=True)
+x_pred = np.load('../study/LPD_COMPETITION/npy/pred.npy', allow_pickle=True)
 
 print(x.shape, y.shape) # (48000, 64, 64, 3) (48000,)
 print(x_pred.shape)     # (72000, 64, 64, 3)
 
-from tensorflow.keras.utils import to_categorical
-y = to_categorical(y)
-# print(y)
-# print(y.shape)  # (48000, 1000)
+# from tensorflow.keras.utils import to_categorical
+# y = to_categorical(y)
+# # print(y)
+# # print(y.shape)  # (48000, 1000)
 
-print(x.shape, y.shape) # (48000, 64, 64, 3) (48000, 1000)
-print(x_pred.shape)     # (72000, 64, 64, 3)
+# print(x.shape, y.shape) # (48000, 64, 64, 3) (48000, 1000)
+# print(x_pred.shape)     # (72000, 64, 64, 3)
 
 from sklearn.model_selection import train_test_split
 x_train, x_val, y_train, y_val = train_test_split(
@@ -40,8 +41,11 @@ model.summary()
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 
-es = EarlyStopping(monitor='loss', patience=30, mode='auto')
-model.fit(x_train, y_train, epochs=100, validation_data=(x_val, y_val), callbacks=[es], batch_size=64)
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+mc = ModelCheckpoint('../study/LPD_COMPETITION/h5/challenge7.hdf5', save_only_true=True, verbose=1)
+es = EarlyStopping(patience=30)
+reduce_lr = ReduceLROnPlateau(patience=15, factor=0.5)
+model.fit(x_train, y_train, epochs=1000, validation_data=(x_val, y_val), callbacks=[mc, es, reduce_lr], batch_size=64)
 
 # predict
 result = model.predict(x_pred, verbose=True)
@@ -49,7 +53,7 @@ result = model.predict(x_pred, verbose=True)
 print(result.shape)
 sub = pd.read_csv('../study/LPD_COMPETITION/sample.csv')
 sub['prediction'] = np.argmax(result,axis = 1)
-sub.to_csv('../study/LPD_COMPETITION/answer.csv',index=False)
+sub.to_csv('../study/LPD_COMPETITION/answer7.csv',index=False)
 
 
 
